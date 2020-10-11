37bdbc26ebd3c856a20afbb66c103343630118a0
jimthomas1997 <jmthms833@gmail.com>
2020-10-11 11:49:54 +0530

    added link to previous html

--- live_clock.py
+++ live_clock.py
@@ -1,18 +1,19 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
 
-# import requests
-# from bs4 import BeautifulSoup
 
-# page=requests.get('https://weather.com/en-IN/weather/today/l/9b6182fe7982a37d6eb65491b0218d1c7e7ee13e0b60e40bfaa9669973e7e933')
-# # print(page)
-# soup=BeautifulSoup(page.content,'html.parser')
-# item1=soup.find('div',class_="_1A9zV")
-# new=item1.find('span',class_="_3KcTQ").get_text()
 @app.route("/")
-@app.route("/home")
+@app.route("/form")
 def home():
-    return render_template('form.html')
+    import requests
+    from bs4 import BeautifulSoup
+
+    page=requests.get('https://weather.com/en-IN/weather/today/l/9b6182fe7982a37d6eb65491b0218d1c7e7ee13e0b60e40bfaa9669973e7e933')
+    # print(page)
+    soup=BeautifulSoup(page.content,'html.parser')
+    item1=soup.find('div',class_="_1A9zV")
+    new=item1.find('span',class_="_3KcTQ").get_text()
+    return render_template('form.html', item=new)
 
 @app.route('/',methods=['POST'])
 def getvalue():
@@ -38,9 +39,16 @@ def getvalue():
         # getting the main dict block
         main = data['main']
         # getting temperature
-        temperature = int(main['temp'] - 273);
+        temperature = int(main['temp'] - 273)
+         # getting the humidity
+        humidity = main['humidity']
+        # getting the pressure
+        pressure = main['pressure']
+        # weather report
+        report = data['weather']
         #end of new code
-        return render_template('home.html', data=temperature,name=name)
+        return render_template('home.html', temp=temperature,name=name,humidity=humidity,
+        pressure=pressure,weather=report[0]['description'])
 
 
 
